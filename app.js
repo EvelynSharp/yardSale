754fc16912640162dea7207574d6c9089898b99d
Evelyn Sharp <yihongshi90@gmail.com>
2017-07-01 19:32:24 -0600

    add sql

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const logger = require('morgan');
@@ -6,6 +7,16 @@ const bodyParser = require('body-parser');
 // const mongoose = require('mongoose');
 // mongoose.connect('mongodb://localhost/yardSale');
 
+const pool = require('./schemas/db');
+
+pool.query('SELECT * FROM products', function(err, res) {
+  if(err) {
+    return console.error('error running query', err);
+  }
+
+  console.log(res.rows);
+});
+
 const app = express();
 
 app.use(logger('dev'));
@@ -13,6 +24,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.resolve(__dirname, './client/build')));
+
+
 
 app.get('*', (request, response) => {
    response.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
---
+++ schemas/db.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+const pg = require('pg');
+//const dbUrl = process.env.DATABASE_URL ? process.env.DATABASE_URL  : 'postgres://localhost:5432/yardSale';
+// const client = new pg.Client(dbUrl);
+// client.connect();
+//
+// const query = client.query('SELECT $1::text as message', ['Hello world!'], (err, res) => {
+//   console.log(err ? err.stack : res.rows[0].message)
+//   client.end()
+// })
+
+const { Pool, Client } = pg;
+const pool = new Pool({
+  user: process.env.PGUSER,
+  host:  process.env.PGHOST,  //TODO:change for web host
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT,
+  max: 10, // max number of clients in the pool
+  idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+})
+
+
+pool.on('error', function (err, client) {
+  console.error('idle client error', err.message, err.stack);
+});
+
+//export the query method for passing queries to the pool
+module.exports.query = function (text, values, callback) {
+  console.log('query:', text, values);
+  return pool.query(text, values, callback);
+};
+
+// the pool also supports checking out a client for
+// multiple operations, such as a transaction
+module.exports.connect = function (callback) {
+  return pool.connect(callback);
+};
