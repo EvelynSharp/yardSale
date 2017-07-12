1722f7e88955dcead7ba28983595bdc01cab93f1
Evelyn Sharp <yihongshi90@gmail.com>
2017-07-12 08:20:09 -0600

    bug fix done()

--- routes/proddetails.js
+++ routes/proddetails.js
@@ -4,7 +4,9 @@ const pool = require('../schemas/db');
 
 router.get('/:id', (req, res) => {
   console.log(req.params.id)
+  console.log("start");
   pool.connect( (err, client, done) => {
+    console.log("test");
     if (err) {
       return console.error('err fetching client from pool', err);
     }
@@ -20,11 +22,11 @@ router.get('/:id', (req, res) => {
       JOIN products p \
         ON p.id=pi.product_id \
       WHERE pi.product_id=$1;', [req.params.id], (err, result) => {
+      done(err);
       if(err) {
         return console.error('error running query', err);
       }
       return res.json(result.rows);
-      done();
     })
   })
 })
